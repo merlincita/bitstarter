5ef71cfb2f37f0359f1c8452641e6a0ca93e8295
Merlin PJ <merlincita_pj@yahoo.es>
2013-07-24 18:25:24 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello World 3!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Hi. Listening on " + port);
 });
