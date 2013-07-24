36e6207f6f183984acd2d2ad0f6e784bdb002137
Merlin PJ <merlincita_pj@yahoo.es>
2013-07-24 19:00:24 +0000

    Reading from file

--- web.js
+++ web.js
@@ -2,11 +2,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    response.send(data);
+  });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Hi. Listening on " + port);
+  console.log("Listening on " + port);
 });
