c6bfe827dedb409f1afe21b08cc5e6f7c800a7e0
Merlin PJ <merlincita_pj@yahoo.es>
2013-07-27 22:06:52 +0000

    updating the port number where the app will be running and the style of index.html

--- web.js
+++ web.js
@@ -4,11 +4,13 @@ app.use(express.logger());
 
 var fs = require('fs');
 
+
 app.get('/', function(request, response) {
   content = fs.readFileSync('index.html', 'utf8');
   var buffer = new Buffer(content, "utf-8");
   response.send(buffer.toString('utf-8'));
 });
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
